af528289ee9516064cda197259076fa53e393888
Yusmin97 <tmdamsi97@gmail.com>
2024-02-29 10:53:25 +0900

    ⚗️ Test: 튜토리얼

--- preload.js
+++ preload.js
@@ -1,10 +1,8 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const type of ['chrome', 'node', 'electron']) {
-      replaceText(`${type}-version`, process.versions[type])
-    }
-  })
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+  // we can also expose variables, not just functions
+})
